10c50548cffcde6eca91dced327cfc20bd79aeed
Тэя Адалинская <teya.adalinskaya@yandex.ru>
2024-09-28 01:00:26 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Адалинская Тэя Валентиновна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Тэя Адалинская, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
