6ebb676dee55b399057a2ce8500575ce7361df89
Тэя Адалинская <teya.adalinskaya@yandex.ru>
2024-09-28 01:30:24 +0700

    Меню

--- app.py
+++ app.py
@@ -2,19 +2,48 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
 <!doctype html>
 <html>
     <head>
-        <title>Адалинская Тэя Валентиновна, лабораторная 1</title>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1>web-сервер на flask</h1>
+        <div>
+            <ul>
+                <li><a href="/lab1">"Первая лабораторная"</li>
+            </ul>
+
+        <footer>
+            &copy; Тэя Адалинская, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, </title>
     </head>
     <body>
         <header>
             НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h1>
+        <h1>Flask — фреймворк для создания веб-приложений на языке программирования Python, 
+        использующий набор инструментов Werkzeug, а также шаблонизатор Jinja2. Относится к 
+        категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+        сознательно предоставляющих лишь самые базовые возможности.</h1>
 
         <footer>
             &copy; Тэя Адалинская, ФБИ-24, 3 курс, 2024
