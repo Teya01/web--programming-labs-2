fd3a43f5127f1f5ec2aecc1155eddf220ae6fc47
Тэя Адалинская <teya.adalinskaya@yandex.ru>
2024-11-09 00:30:21 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -120,6 +120,13 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'teya', 'password': '456'},
+    {'login': 'natalie', 'password': '789'},
+]
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -128,8 +135,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if (login == 'alex' and password == '123') or (login == 'teya' and password == '456') or (login == 'john' and password == '789'):
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
