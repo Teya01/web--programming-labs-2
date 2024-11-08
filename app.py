4f7af02904734827015c5818f2f553a324f695ce
Тэя Адалинская <teya.adalinskaya@yandex.ru>
2024-11-08 18:17:49 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1) 
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/index")
@@ -46,6 +48,12 @@ def menu():
             <div>
                 <ul>
                     <li><a href="/lab3">Третья лабораторная</a></li>
+                </ul>
+            </div>
+
+             <div>
+                <ul>
+                    <li><a href="/lab4">Четвёртая лабораторная</a></li>
                 </ul>
             </div>
 
