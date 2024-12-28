d3f24fb3828e9143eeb57bfcf71c722cc8c81a07
Тэя Адалинская <teya.adalinskaya@yandex.ru>
2024-12-28 17:56:45 +0700

    Лабораторная работа 7

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 from lab9 import lab9
 import os
 
@@ -19,6 +20,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 app.register_blueprint(lab9)
 
 @app.route("/")
@@ -77,6 +79,12 @@ def menu():
             <div>
                 <ul>
                     <li><a href="/lab6">Шестая лабораторная</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li><a href="/lab7">Седьмая лабораторная</a></li>
                 </ul>
             </div>
 
